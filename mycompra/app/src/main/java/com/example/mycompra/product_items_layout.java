8933ca68db9ff68d60a4d299b278df3d6d491348
icaro <icaroiraku@gmail.com>
2020-06-28 20:08:27 -0300

    trabalho thyago

---
+++ mycompra/app/src/main/java/com/example/mycompra/product_items_layout.java
@@ -0,0 +1,14 @@
+package com.example.mycompra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class product_items_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_items_layout);
+    }
+}
